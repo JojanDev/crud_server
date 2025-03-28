e23c5c08644ae82243dc0f62468b19aa8c1bf68e
Johan Delgado <johan24.dev@gmail.com>
2025-03-28 17:13:48 -0500

    custom endpoints

--- Server/Api/server.js
+++ Server/Api/server.js
@@ -4,6 +4,20 @@ const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+
+server.use(
+  jsonServer.rewriter({
+    "/asesinos": "/killers",
+    "/supervivientes": "/survivors",
+    "/mapas": "/maps",
+    "/habilidades": "/perks",
+    "/asesinos/:id": "/killers/:id",
+    "/supervivientes/:id": "/survivors/:id",
+    "/mapas/:id": "/maps/:id",
+    "/habilidades/:id": "/perks/:id",
+  })
+);
+
 server.use(router);
 server.listen(3000, () => {
   console.log("JSON Server is running");
